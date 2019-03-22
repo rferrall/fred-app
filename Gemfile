6c43ee58861c48dbcdd4a42b36fd4308fad72d8c
rachel <ferrallr@yahoo.com>
2019-03-22 16:24:46 -0500

    adds phone_number column on user table, and twilio-ruby gem to send text message notifications when a new message has been created

--- Gemfile
+++ Gemfile
@@ -50,3 +50,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'http'
 gem 'jwt'
 gem 'rack-cors', require: 'rack/cors'
+gem 'twilio-ruby'
+gem 'figaro'
--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,6 @@
 class Api::MessagesController < ApplicationController
 
+require 'twilio-ruby'
 
 
   def create
@@ -9,6 +10,15 @@ class Api::MessagesController < ApplicationController
       conversation_id: params[:conversation_id]
       )
     if @message.save
+
+      account_sid = ENV["ACCOUNT_SID"]
+      auth_token = ENV["AUTH_TOKEN"]   
+
+      @client = Twilio::REST::Client.new account_sid, auth_token
+      message = @client.messages.create(
+          body: "You have a new message from Fred",
+          to: "+17632422868",    
+          from: "+19523334671")  
       render 'show.json.jbuilder'
     else
       render json: {errors: @message.errors.full_messages}, status: :unprocessable_entity
