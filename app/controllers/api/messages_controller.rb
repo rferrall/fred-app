e15cf4d23309185bf82ef9e9d3c11698c6e7fe3f
rachel <ferrallr@yahoo.com>
2019-03-25 15:31:45 -0500

    adds twilio logic to pull message receivers phone number

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -12,13 +12,21 @@ require 'twilio-ruby'
     if @message.save
 
       account_sid = ENV["ACCOUNT_SID"]
-      auth_token = ENV["AUTH_TOKEN"]   
-
-      @client = Twilio::REST::Client.new account_sid, auth_token
-      message = @client.messages.create(
-          body: "You have a new message from Fred",
-          to: "+17632422868",    
-          from: "+19523334671")  
+      auth_token = ENV["AUTH_TOKEN"] 
+      conversation = @message.conversation  
+      if conversation.sender_id == current_user.id
+        twilio_phone_number = conversation.recipient.phone_number
+      else
+        twilio_phone_number = conversation.sender.phone_number
+      end
+      if twilio_phone_number
+        @client = Twilio::REST::Client.new account_sid, auth_token
+        message = @client.messages.create(
+            body: "Alert from Fred => You have a new message from #{current_user.name}",
+            to: twilio_phone_number,    
+            from: "+19523334671") 
+      end
+       
       render 'show.json.jbuilder'
     else
       render json: {errors: @message.errors.full_messages}, status: :unprocessable_entity
